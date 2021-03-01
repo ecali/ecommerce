b0a48b7dc9792bc18e69a33a59851b2ae677eb07
Elvio Cali <info@ecali.it>
2021-03-01 12:04:48 +0100

    add directory hompage compoment

---
+++ src/Components/Directory/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import bakShop from './../../assets/bakery.jpg'
+import pizShop from './../../assets/pizza.jpg'
+
+import './style.scss'
+const Directory = props =>{
+    return(
+        <div className='directory'>
+            <div className='wrap'>
+                <div className='item' style={{backgroundImage:`url(${bakShop})`}}>
+                    <a>
+                        Shop from Bakery
+                    </a>
+                </div>
+                <div className='item' style={{backgroundImage:`url(${pizShop})`}}>
+                    <a>
+                        Shop from Pizzeria
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Directory
--- src/Pages/Hompage/index.js
+++ src/Pages/Hompage/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Directory from './../../Components/Directory'
+import './style.scss'
 const Homepage = props =>{
     return(
-        <section>
-            <h1>Homepage page</h1>
+        <section className='homepage'>
+            <Directory />
         </section>
     )
 }
